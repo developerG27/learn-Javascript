4de331525814484c3f7c7989700403f7ac313445
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-20 14:50:03 +0100

    introduzione jquery

---
+++ funzioni-matematiche.js
@@ -0,0 +1,4 @@
+/*
+https://www.w3schools.com/js/js_math.asp
+*/
+console.log(Math.ceil(Math.random()*1000))
---
+++ jquery/introduzione.js
@@ -0,0 +1,14 @@
+/*
+E' una libreria di Javascript che aumenta la produttività quando scrivi codice
+Molti sviluppatori dicono che jquery è morto, questo è vero fino ad un certo punto, perchè il web moderno usa ancora tantissimo jquery, se non per nuovi progetti, per mantenere quelli già in produzione.
+Quello che si critica maggiormente a jquery è che molti sviluppatori ne hanno abusato inserendo la libreria anche in progetti molto semplici che non ne avevano bisogno
+
+INDICE:
+Selettori
+Eventi
+Effetti
+Ajax
+Jquery UI
+Animazioni
+Plugin e Widget
+*/
