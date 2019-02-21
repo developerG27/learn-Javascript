1c91452e32cd0c7469ddc878ef694c91ad6f3424
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-21 12:47:41 +0100

    commentato esercizio textare: salva il valore della textarea in un <li>

--- progetti/textarea-storage/assets/js/app.js
+++ progetti/textarea-storage/assets/js/app.js
@@ -1,47 +1,21 @@
-//Variabili
-const listaTweets = document.getElementById('lista-tweets')
+//salvo in una costante il form
+const form = document.getElementById('formulario')
 
-//Event Listener
-eventListener()
-function eventListener(){
-    //l'elemento con id #formulario tiene traccia dell'evento submit
-    document.querySelector('#formulario').addEventListener('submit',agregarTweet)
+//aggiungo una funzione che si eseguirà all'evento submit
+form.addEventListener('submit', aggiungereTweet)
 
-    //eliminare tweet
-    listaTweets.addEventListener('click',borrarTweet)
-}
-
-//Function
-function agregarTweet(e){
-    //Previene l'azione di default
-    e.preventDefault()
-    console.log('Formulario inviato')
-    //Prende il contenuto di #tweet
+//creo la funzione e gli aggiungo come parametro un evento
+function aggiungereTweet(evento){
+    //elimino l'evento che il browser fa di default
+    evento.preventDefault()
+    //salvo in una costante il contenuto della textarea
     const tweet = document.getElementById('tweet').value
-    //creo un elemento li
+    //salvo in una costante il contenitore dei tweet
+    const lista_tweets = document.getElementById('lista-tweets')
+    //inizializzo un elemento <li>
     const li = document.createElement('li')
-    //a li gli aggiungo il testo contenuto in tweet
+    //al tag <li> gli aggiungo il testo del valore della textarea
     li.innerText = tweet
-    //<li> viene aggiunto al DOM
-    listaTweets.appendChild(li)
-
-    //creare button per eliminare
-    const buttonDelate = document.createElement('a')
-    //Aggiungere una classe
-    buttonDelate.classList = 'borrar-tweet'
-    //aggiunge il suo contenuto
-    buttonDelate.innerText = 'X'
-    //aggiungi il button alla lista del DOM
-    li.appendChild(buttonDelate)
-
-    console.log(tweet)
-}
-
-function borrarTweet(e){
-    e.preventDefault()
-    if(e.target.className === 'borrar-tweet'){
-        console.log(e.target.parentElement.remove())
-        console.log('Tweet Eliminato')
-    }
-    console.log('Hai fatto click alla lista')
+    //Al contenitore dei tweet gli appendo il tag <li>
+    lista_tweets.appendChild(li)
 }
