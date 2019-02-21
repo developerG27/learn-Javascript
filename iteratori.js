01e8874a765b7834ceb5b7962c0e343f244999bf
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-21 09:18:52 +0100

    Sezione iteratori

---
+++ iteratori.js
@@ -0,0 +1,12 @@
+/*
+Un iteratore è un oggeto in grado di processare una serie di collezioni
+*/
+const paesi = ['Italia','Australia','Nuova Zelanda','Svizzera']
+for(let entrate of paesi.entries()){
+    console.log(entrate)
+}
+
+const numeri = [233,543,12,456,32,55,89]
+for(let entrate of numeri){
+    console.log(entrate)
+}
