05fda804df072a479c451dac2d65d01b07da88b5
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-21 10:26:55 +0100

    Aggiunto materiale in scripting

--- scripting.js
+++ scripting.js
@@ -4,3 +4,32 @@ let immaginiArray = Array.from(immagini)
 immaginiArray.forEach(function(immagini){
     console.log(immagini)
 })
+
+
+//Traversing è potersi introdurre a qualsiasi elemento del DOM e muoversi in qualsiasi direzione
+const navigazione = document.querySelector('#navigazione')
+console.log(navigazione.children)
+
+
+//Creare elementi da Javascript
+const link = document.createElement('a')
+//aggiungere la classe
+link.className = 'link'
+//aggiungere id
+link.id = 'nuovo-id'
+//aggiungere un attributo
+link.setAttribute('href','#')
+//aggiungere testo
+link.appendChild(document.createTextNode('Nuovo link'));
+//aggiungere al dom
+document.body.appendChild(link)
+console.log(link)
+
+//Rimpiazzare elementi del DOM
+const nuovoH1 = document.createElement('h1')
+nuovoH1.id = 'header'
+nuovoH1.appendChild(document.createTextNode('I migliori corsi'))
+const precedenteH1 = document.querySelector('#vecchio-h1')
+const contenitoreH1 = document.querySelector('con-h1')
+//Rimpiazza
+contenitoreH1.replaceChild(precedenteH1, nuovoH1)
