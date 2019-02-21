caaaa2821371376862967287f1a74886f52b07ee
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-21 16:32:03 +0100

    textarea collegato al local storage

--- progetti/textarea-storage/assets/js/app.js
+++ progetti/textarea-storage/assets/js/app.js
@@ -7,6 +7,7 @@ const lista_tweets = document.getElementById('lista-tweets')
 form.addEventListener('submit', aggiungereTweet)
 //funzione che si eseguirà all'evento click del contenitore tweet
 lista_tweets.addEventListener('click',delateTweet)
+document.addEventListener('DOMContentLoaded', localStoragePronto)
 
 //creo la funzione che aggiunge i tweet e gli aggiungo come parametro un evento
 function aggiungereTweet(evento){
@@ -14,14 +15,12 @@ function aggiungereTweet(evento){
     evento.preventDefault()
     //salvo in una costante il contenuto della textarea
     const tweet = document.getElementById('tweet').value
-
     //creo bottone di eliminazione
     const delate = document.createElement('a')
     //gli aggiungo una classe
     delate.classList = 'delate'
     //aggiungo il suo contenuto
     delate.innerText = 'X'
-
     //crea un tag <li>
     const li = document.createElement('li')
     //al tag <li> gli aggiungo il testo del valore della textarea
@@ -30,15 +29,85 @@ function aggiungereTweet(evento){
     li.appendChild(delate)
     //Al contenitore dei tweet gli appendo il tag <li>
     lista_tweets.appendChild(li)
+    //aggiungere al local storage
+    aggiungereTweetLocalStorage(tweet)
 }
 
-//funzione che cancella il tweet
+//funzione che cancella il tweet dal DOM
 function delateTweet(event){
     //disattivo l'evento di default
     event.preventDefault()
     //Se il nome delal classe dell'evento è delate
     if(event.target.className === 'delate'){
         //rimuove l'elemento
-        console.log(event.target.parentElement.remove())
+        event.target.parentElement.remove()
+        delateTweetLocalStorage(event.target.parentElement.textContent)
     }
+}
+
+//Mostrare dati dal local storage
+function localStoragePronto(){
+    let tweets
+    tweets = ottenereTweetLocalStorage()
+
+    tweets.forEach(function(tweet){
+        //creo bottone di eliminazione
+        const delate = document.createElement('a')
+        //gli aggiungo una classe
+        delate.classList = 'delate'
+        //aggiungo il suo contenuto
+        delate.innerText = 'X'
+
+        //crea un tag <li>
+        const li = document.createElement('li')
+        //al tag <li> gli aggiungo il testo del valore della textarea
+        li.innerText = tweet
+        //al tag <li> aggiunto il bottone delate
+        li.appendChild(delate)
+        //Al contenitore dei tweet gli appendo il tag <li>
+        lista_tweets.appendChild(li)
+    })
+}
+
+//aggiungere tweeet al local storage
+function aggiungereTweetLocalStorage(tweet){
+    let tweets;
+    //faccio partire la funzione
+    tweets = ottenereTweetLocalStorage()
+
+    //aggiungere il nuovo all'interno dell'array
+    tweets.push(tweet)
+
+    //Convertire da array a string prima di inserirlo in local storage
+    localStorage.setItem('tweets',JSON.stringify(tweets))
+}
+
+//controlla se ci sono elementi nel local Storage, ritorna un array
+function ottenereTweetLocalStorage(){
+    let tweets;
+    //Se nel local storage non esiste 'tweets'
+    if(localStorage.getItem('tweets') === null){
+        //ritorna un array vuoto
+        tweets = []
+    } else{
+        tweets = JSON.parse(localStorage.getItem('tweets'))
+    }
+    return tweets
+}
+
+//eliminare tweet dal local storage
+function delateTweetLocalStorage(tweet){
+    let tweets, tweetCancellato
+    //Elimina la x dal tweet
+    tweetCencellato = tweet.substring(0, tweet.length - 1)
+
+    tweets = ottenereTweetLocalStorage()
+
+    tweets.forEach(function(tweet, index){
+        if(tweetCencellato === tweet){
+            tweets.splice(index, 1)
+        }
+    })
+
+    localStorage.setItem('tweets', JSON.stringify(tweets))
 }
