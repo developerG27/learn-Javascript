1da4174af4282074cf652dfd21ec04cb79bf8f90
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-21 11:06:00 +0100

    Aggiunto progetto textarea

---
+++ progetti/textarea-storage/assets/js/app.js
@@ -0,0 +1,35 @@
+//Variabili
+const listaTweets = document.getElementById('lista-tweets')
+
+//Event Listener
+eventListener()
+function eventListener(){
+    //l'elemento con id #formulario tiene traccia dell'evento submit
+    document.querySelector('#formulario').addEventListener('submit',agregarTweet)
+}
+
+//Function
+function agregarTweet(e){
+    //Previene l'azione di default
+    e.preventDefault()
+    console.log('Formulario inviato')
+    //Prende il contenuto di #tweet
+    const tweet = document.getElementById('tweet').value
+    //creo un elemento li
+    const li = document.createElement('li')
+    //a li gli aggiungo il testo contenuto in tweet
+    li.innerText = tweet
+    //<li> viene aggiunto al DOM
+    listaTweets.appendChild(li)
+
+    //creare button per eliminare
+    const buttonDelate = document.createElement('a')
+    //Aggiungere una classe
+    buttonDelate.classList = 'borrar-tweet'
+    //aggiunge il suo contenuto
+    buttonDelate.innerText = 'X'
+    //aggiungi il button alla lista del DOM
+    li.appendChild(buttonDelate)
+
+    console.log(tweet)
+}
