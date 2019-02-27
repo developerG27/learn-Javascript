02212c2dc28df60c1df8890e71b4b382fa10a895
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-27 16:00:07 +0100

    caratteristiche

--- cos'è-javascript.js
+++ cos'è-javascript.js
@@ -30,17 +30,32 @@ Javascript è molto di più in quanto al suo interno troviamo:
 - BOM (Browser Object Model)
 EcmaScript non è legato al web in quanto definisce solo la sintassi del linguaggio, questo ha portato a estrapolarlo dal web e così sono nate nuove tecnologie come Node.js
 
-EcmaScript
+EcmaScript (ES)
 Ecma sta per Eurpean Computer Manufacturers Association.
 Negli anni si è evoluto:
--prima versione: è la stessa che fu prodotta da Brendan Eich
--seconda versione: ci furono piccolissime cambiamenti
--terza versione: permise un salto di qualità in quanto portò Javascript come lo conosciamo oggi
--quarta versione: ebbe un cambiamento troppo radicale, furono introdotte variabili fortemente tipizzate, classi, ereditarietà per questo motivo fu presto abbandonata
--quinta versione: pubblicata nel 2009, aggiunge i JSON, la strict mode
--sesta vesione - ES6: pubblicata nel 2015, si sono aggiunte le classi, sono stati aggiunti i rest e spread operator, destructuring
--settimaa versione ES7
+-ES1: Giugno 1997, è la stessa che fu prodotta da Brendan Eich
+-ES2: Giugno 1998, ci furono piccolissime cambiamenti
+-ES3: Dicembre 1999, permise un salto di qualità in quanto portò Javascript come lo conosciamo oggi
+-ES4: ebbe un cambiamento troppo radicale, furono introdotte variabili fortemente tipizzate, classi, ereditarietà per questo motivo fu presto abbandonata.
+-ES5: Dicembre 2009, aggiunge i JSON, la strict mode
+-ES6: Giugno 2015, si sono aggiunte le classi, sono stati aggiunti i rest e spread operator, destructuring
+-ES7: Giugno 2016
+-ES8: Giugno 2017
+-ES9: Giugno 2018
+-ES10
 
 Caratteristiche
-Non ha bisogno di essere compilato in quanto viene interpretato dai browser
+E' un linguaggio di programmazione multi paradigma, questo vuol dire che può essere:
+- Orientato ad oggetti basato su prototipi (il più comune)
+- Funzionale
+- Interpretato
+- Imperativo
+Non ha bisogno di essere compilato in quanto viene interpretato dal browser,
+Non è obbligatorio inserire il punto e virgola
+
+console.log('hello world')
+
+NaN : not a number
+Non è uguale a niente, nemmeno a se stesso
 */
+
