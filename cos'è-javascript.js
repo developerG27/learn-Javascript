4ddf28f8e192e730f7c4d317c0a4aadc3ee785a8
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-27 15:15:14 +0100

    Storia di Javascript

--- cos'è-javascript.js
+++ cos'è-javascript.js
@@ -1,20 +1,25 @@
 /*
-Cos'è Javascript?
-Javascript è un linguaggio di programmazione che permette a gli sviluppatori manipolare il comportamento di un sito web.
-Sebbene sia un linguaggio facile da imparare, la sua conoscenza avanzata richiede più tempo
-E' un linguaggio incorporato all'interno del browser
-Javascript non è Java.
+A cosa serve Javascript?
+Javascript è un linguaggio di programmazione che permette a gli sviluppatori manipolare il comportamento di un sito web, negli ultimi anni però, la sua evoluzione è stata talmente esponenziale che ha influenzato quasi tutti i rami dell'informatica.
 
-Javascript è un linguaggio con un potenziale incredibile ed è quello con maggior sviluppo in questo momento.
+E' in assoluto il linguaggio protagonista del web moderno in quanto permette di:
+- Manipolare gli elementi all'interno del browser
+- Validazione dei form
+- Chat
+- Webscraping
+- Applicazioni lato server
 
-Dove funziona Javascript?
-Javascript è il linguaggio protagonista del web, si usa per creare
-e-mail
-Chat
-Ricercatori
-Fare streaming
-Internet of Things
+Ma il suo uso non si ferma solo al web, si può anche
+- Programmare l' Internet Of Thing (Internet delle cose)
+- Creare software per Windows, Mac, Linux (Electron)
+- Sviluppo di applicazioni native per Android e iOS (React Native)
+- Creare Videogiochi online (canvas)
 
+
+La storia di Javascript
+Fu creato da Brendan Eich nel 1995
+Il primo nome di questo linguaggio fu Mocha, poi venne sostituito da LiveScript e successivamente, tramite accordi commerciali si arrivo a Javascript.
+Javascript non è Java, non condivide praticamente nulla se non che entrambi sono linguaggi di programmazione.
 
 Caratteristiche
 Non ha bisogno di essere compilato in quanto viene interpretato dai browser
