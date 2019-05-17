568a8cbb82197ff96ff0080bd94fcfe971e89501
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-05-17 16:26:54 +0200

    Class

---
+++ POO/class.js
@@ -0,0 +1,13 @@
+class Usuario {
+  constructor(nombre, apellido, correo, edad){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.correo = correo
+    this.edad = edad
+  }
+}
+
+let beto = new Usuario('Gian', 'Cognome','gian.pumayalla@gmail.com',23);
+
+
+console.log(beto);
---
+++ POO/oggetto.js
@@ -0,0 +1,20 @@
+//Client napssive
+const clienteNapssive = {
+  nome : 'Gian',
+  cognome: 'Pumayalla',
+  email: 'gian.pumayalla@gmail.com',
+  pass: 'f%$fbkdsuf&3',
+}
+
+//Accedere all'informazione degli oggetti
+console.log(clienteNapssive.nome);
+
+
+//Cliente palestra
+const clientGym = {
+  name: 'Beto',
+  age: 28,
+  weight: 85,
+  height: 175,
+  registered: "22/02/2019"
+}
