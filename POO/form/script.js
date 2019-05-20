2aa75344dbf19141c9da5901903eae5f914854ff
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-05-20 10:21:37 +0200

    valori del form

--- POO/form/script.js
+++ POO/form/script.js
@@ -1,6 +1,27 @@
+class Utente {
+  constructor(nome,cognome,email,password){
+    this.nome = nome
+    this.cognome = cognome
+    this.email = email
+    this.password = password
+  }
+}
+
 let form1 = document.getElementById('formulario');
 
 form1.addEventListener('submit', e =>{
   e.preventDefault()
-  console.log(e.target.nome.value)
+  let nome = e.target.nome.value
+  let cognome = e.target.cognome.value
+  let email = e.target.email.value
+  let password = e.target.password.value
+
+  let utente = new Utente(
+    nome,
+    cognome,
+    email,
+    password
+  )
+
+  console.log(utente);
 });
