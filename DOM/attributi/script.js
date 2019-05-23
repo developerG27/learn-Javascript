db625c9d2b4797ee01e4e26e281f199c39720ec0
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-05-23 07:57:19 +0200

    inizio dom

--- DOM/attributi/script.js
+++ DOM/attributi/script.js
@@ -1,16 +1,24 @@
+//selezionare dal DOM un elemento con il id title
 const title = document.getElementById('title')
 console.log(title.getAttribute('id'))
 
+//selezionare dal DOM gli elemeenti h2
 const subtitle = document.querySelector('h2')
+
+//inserire l'id all'elemento
 subtitle.setAttribute('id','subtitle')
 
+//inserire la classe all'elemento
 title.classList.add('main','title')
 
-console.log(title.textContent)
-
-
-//Creare elementi
+//Creare il tag p
 const paragrafo = document.createElement('p')
+
+//Cambiare il testo di paragrafo
 paragrafo.textContent = "Sono un paragrafo"
+
+//Settare id paragrafo
 paragrafo.id = 'teacher'
+
+//Inserire nel DOM paragrafo
 document.body.appendChild(paragrafo)
