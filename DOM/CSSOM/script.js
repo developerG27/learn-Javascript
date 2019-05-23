0a0177360f42199cdd37f4159c6e3c633284da05
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-05-23 15:08:43 +0200

    media query con js

--- DOM/CSSOM/script.js
+++ DOM/CSSOM/script.js
@@ -2,4 +2,19 @@ const title = document.querySelector('h1')
 const description = document.querySelector('p')
 
 //modifica lo stile
-title.style.color = 'red';
+title.style.color = 'blue'
+
+
+const mediumBp = matchMedia('(min-width: 640px) and (orientation: portrait)')
+console.log(mediumBp);
+
+const changeColor = () =>{
+  if(mediumBp.matches){
+    document.body.style.background = 'red'
+  } else{
+    document.body.style.background = 'yellow'
+  }
+}
+
+addEventListener('DOMContentLoaded',changeColor)
+addEventListener('resize', changeColor)
