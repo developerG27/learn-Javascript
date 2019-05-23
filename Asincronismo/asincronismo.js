bddede216a6d0cdb117121df594b957fcd370358
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-05-23 07:49:51 +0200

    asincronismo

--- Asincronismo/asincronismo.js
+++ Asincronismo/asincronismo.js
@@ -5,13 +5,3 @@ setTimeout(function(){
 console.log('c')
 
 setTimeout(() => console.log('d'),2000)
-
-
-
-
-/**
- Async Await è la forma più semplice di realizzare azioni asincrone
- E' importante pensare agli array quando si usano async await
- Async Await ferma l'esecuzione del programma fin quando tutte le promesse non vengono risolte
- */
-
