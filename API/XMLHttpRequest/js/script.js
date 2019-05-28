76df2136fe738f72900c0c6cd6138dbb25e11e39
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-05-28 22:54:19 +0200

    loading

--- API/XMLHttpRequest/js/script.js
+++ API/XMLHttpRequest/js/script.js
@@ -1,8 +1,15 @@
 const c = document.getElementById('myContent')
 const b = document.getElementById('btnLoad')
+const l = document.getElementById('loading')
+
+l.style.display = 'none'
 
 b.addEventListener('click', e => {
+  l.style.display = 'block'
   const xhr = new XMLHttpRequest();
+  console.log(xhr.readyState);
+
+
   // xhr.open('metodo','dove', 'asincrona o no')
   xhr.open('GET','./data.html', true)
 
@@ -10,6 +17,7 @@ b.addEventListener('click', e => {
   xhr.addEventListener('load', e => {
     console.log(e.target)
     c.innerHTML = e.target.responseText
+    l.style.display = 'none'
   })
 
   // Realizzare la richiesta
